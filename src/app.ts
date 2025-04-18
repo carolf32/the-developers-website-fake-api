22b21e33b051ab9ca5a9dfbb37013079e9b9146e
Carolina do Amaral <carolinafrederico32@hotmail.com>
2025-04-18 17:18:39 +0100

    cors intalled

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import "reflect-metadata";
 import { commentRouter } from "./routes/commentRouter";
 import { userRouter } from "./routes/userRouter";
 import { HandleErrors } from "./middlewares/handleErrors.middleware";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api", commentRouter);
 app.use("/api/users", userRouter);
 
