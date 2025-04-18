97395b7a8e7d13830f0035230578544f2f236869
Carolina do Amaral <carolinafrederico32@hotmail.com>
2025-04-18 19:53:37 +0100

    update prisma client

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -161,11 +161,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://carol:adelia@localhost:5432/the-developers-website-db?schema=public"
+        "value": null
       }
     }
   },
